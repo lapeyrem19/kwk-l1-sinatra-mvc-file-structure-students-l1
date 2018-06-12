347863298237345bd10a7c3a1116a491669e403c
lapeyrem19 <lapeyrem19@ashevilleschool.org>
2018-06-12 18:32:35 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -6,21 +6,21 @@ def initialize (name,breed,age)
 end
 
     def name= (name)
-      @name= name
+      @name = name
     end
 
     def name
       @name
 
     def breed= (breed)
-      @breed= breed
+      @breed = breed
     end
 
     def breed
     @breed
 
     def age= (age)
-      @age= age
+      @age = age
     end
 
     def age
