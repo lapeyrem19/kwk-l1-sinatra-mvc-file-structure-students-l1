c0f7b47b5085e4c3cbdf5402d29f04e27533f7ec
lapeyrem19 <lapeyrem19@ashevilleschool.org>
2018-06-12 18:29:56 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,4 +1,30 @@
 class Dog
+def initialize (name,breed,age)
+  @name = name
+  @breed = breed
+  @age = age
+end
+
+    def name= (name)
+      @name= name
+    end
+
+    def name
+      @name
+
+    def breed= (breed)
+      @breed= breed
+    end
+
+    def breed
+    @breed
+
+    def age= (age)
+      @age= age
+    end
+
+    def age
+      @age
 
 
 end
